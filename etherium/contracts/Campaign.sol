cac7733b5675fa75b7e5ae792f08ed0afac8f67d
SHEFFmachine <sheff@sheffmachine.com>
2018-07-21 14:16:46 -0700

    Set up the basic kickstarter etherium project

---
+++ etherium/contracts/Campaign.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+    
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+    
+    function getDeployedCampaigns() public view returns(address[]) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recepient;
+        bool complete;
+        uint approverCount;
+        mapping(address => bool) approvals;
+    }
+    
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+    Request[] public requests;
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+    
+    function contribute() public payable restricted {
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+    
+    function createRequest(string description, uint value, address recepient) 
+        public restricted 
+    {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recepient: recepient,
+            approverCount: 0,
+            complete: false
+        });
+        
+        requests.push(newRequest);
+    }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        
+        
+        require(approvers[msg.sender]);
+        require(!requests[index].approvals[msg.sender]);
+        
+        request.approvals[msg.sender] = true;
+        request.approverCount++;
+        
+    }
+    
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        require(!request.complete);
+        require(request.approverCount > approversCount / 2);
+        
+        request.recepient.transfer(request.value);
+        request.complete = true;
+        
+    }
+}
