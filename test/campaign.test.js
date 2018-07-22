be56e1d65fa188bd10d223d5a57b27078db447e0
SHEFFmachine <sheff@sheffmachine.com>
2018-07-21 18:25:09 -0700

    Write more tests

--- test/campaign.test.js
+++ test/campaign.test.js
@@ -71,4 +71,32 @@ describe('Campaigns', () => {
 
     assert.equal('Buy batteries', request.description)
   })
+
+  it('processes requests', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    })
+
+    await campaign.methods
+      .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({ from: accounts[0], gas: '1000000' })
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    })
+
+    await campaign.methods
+      .finalizeRequest(0)
+      .send({ from: accounts[0], gas: '1000000' })
+
+    let balance = await web3.eth.getBalance(accounts[1]) //string in wei
+    balance = web3.utils.fromWei(balance, 'ether')
+    balance = parseFloat(balance)
+
+    console.log(balance)
+
+    assert(balance > 104) // the best we can do so far.
+  })
 })
