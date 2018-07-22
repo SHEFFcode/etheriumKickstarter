90e31a56cb2ed2f0557d402980c6cc6e1abb73fc
SHEFFmachine <sheff@sheffmachine.com>
2018-07-21 20:03:35 -0700

    basic initial page done

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import factory from '../etherium/factory'
 
 class CampaignIndex extends Component {
-  componentDidMount = async () => {
+  // Because of server side rendering we do not get componentDidMount
+  // We have to use getInitialProps used by next.js
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call()
-    console.log(campaigns)
+    return { campaigns }
   }
 
   render() {
-    return <div>Campaigns Index</div>
+    return <div>{this.props.campaigns[0]}</div>
   }
 }
 
