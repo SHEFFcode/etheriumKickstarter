ff57c10530cba804f5693791642b3f4a49dab122
SHEFFmachine <sheff@sheffmachine.com>
2018-07-21 18:36:51 -0700

    Write deployment

---
+++ etherium/deploy.js
@@ -0,0 +1,23 @@
+const compiledFactory = require('./build/CampaignFactory.json')
+const env = require('./env.js')
+
+const HDWalletProvier = require('truffle-hdwallet-provider')
+const Web3 = require('web3')
+
+const provider = new HDWalletProvier(env.mneumonic, env.address)
+const web3 = new Web3(provider)
+
+async function deploy() {
+  const accounts = await web3.eth.getAccounts()
+
+  console.log(`Attempting to deploy a contract from account ${accounts[0]}`)
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: '0x' + compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] })
+
+  console.log(`Contract deployed to ${result.options.address}`)
+}
+deploy()
---
+++ etherium/env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mneumonic:
+    'until depth buzz order immense document salt reflect prison dirt drip walk',
+  address: 'https://rinkeby.infura.io/6C8MmDXWKyMmUN8aNQ0r'
+}
