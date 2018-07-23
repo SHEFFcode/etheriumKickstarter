61212ab167b75de2faf8b04abf53ad84da02bbe6
SHEFFmachine <sheff@sheffmachine.com>
2018-07-22 22:53:26 -0700

    Add a card to the UI to see the running campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Card } from 'semantic-ui-react'
 import factory from '../etherium/factory'
 
 class CampaignIndex extends Component {
@@ -9,8 +10,28 @@ class CampaignIndex extends Component {
     return { campaigns }
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      }
+    })
+
+    return <Card.Group items={items} />
+  }
+
   render() {
-    return <div>{this.props.campaigns[0]}</div>
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"
+        />
+        <div>{this.renderCampaigns()}</div>
+      </div>
+    )
   }
 }
 
