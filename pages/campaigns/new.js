fd8b47f11e42043bc22ab27a7000a2518d34621e
SHEFFmachine <sheff@sheffmachine.com>
2018-07-28 16:08:39 -0700

    hook up a form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import Layout from '../../components/Layout'
+import { Button, Form, Input } from 'semantic-ui-react'
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: ''
+  }
+
   render() {
     return (
       <Layout>
-        <h1>New Campaign</h1>
+        <h3>Create a campaign</h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label={'wei'}
+              labelPosition={'right'}
+              value={this.state.minimumContribution}
+              onChange={e =>
+                this.setState({ minimumContribution: e.target.value })
+              }
+            />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
       </Layout>
     )
   }
