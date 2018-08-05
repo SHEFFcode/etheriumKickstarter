c490589cbfd7b492664a7e5558c3f27fc1746e09
SHEFFmachine <sheff@sheffmachine.com>
2018-08-04 18:12:06 -0700

    We have a fully finished request displayer

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
 import { Link } from '../../../routes'
 import Layout from '../../../components/Layout'
+import Campaign from '../../../etherium/Campaign'
 
 class RequestIndexPage extends Component {
   static async getInitialProps(props) {
     const { address } = props.query
-    return { address }
+    const campaign = Campaign(address)
+    const requestCount = await campaign.methods.getRequestsCount.call()
+    const requests = await Promise.all(
+      Array(requestCount)
+        .fill()
+        .map((el, index) => {
+          return campaign.methods.requests(index).call()
+        })
+    )
+    return { address, requests, requestCount }
   }
   render() {
     const { address } = this.props
