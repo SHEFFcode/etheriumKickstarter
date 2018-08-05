7f2c909b7d430e4ffd01e5b4679a4aec821af55d
SHEFFmachine <sheff@sheffmachine.com>
2018-08-04 17:22:32 -0700

    start working on new page for requests

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react'
+import { Form, Button, Message, Input } from 'semantic-ui-react'
+import Campaign from '../../../etherium/Campaign'
+import web3 from '../../../etherium/web3'
+import { Link, Router } from '../../../routes'
 
 class RequestNew extends Component {
+  state = {
+    value: '',
+    description: '',
+    recepient: ''
+  }
+  static async getInitialProps(props) {
+    const { address } = props.query
+    return { address }
+  }
   render() {
-    return <h3>Create a requests</h3>
+    const { description, recepient, value } = this.state
+    return (
+      <Form>
+        <Form.Field>
+          <Label>Description</Label>
+          <Input
+            value={description}
+            onChange={e => this.setState({ description: e.target.value })}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Label>Value (ether)</Label>
+          <Input
+            value={value}
+            onChange={e => this.setState({ value: e.target.value })}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Label>Recepient</Label>
+          <Input
+            value={recepient}
+            onChange={e => this.setState({ value: e.target.recepient })}
+          />
+        </Form.Field>
+      </Form>
+    )
   }
 }
 
