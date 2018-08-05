14c82d050ecb85f8f2405220015c6a41f9f072b7
SHEFFmachine <sheff@sheffmachine.com>
2018-08-04 18:20:02 -0700

    Set up the table

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Table, Tab } from 'semantic-ui-react'
 import { Link } from '../../../routes'
 import Layout from '../../../components/Layout'
 import Campaign from '../../../etherium/Campaign'
@@ -10,7 +10,7 @@ class RequestIndexPage extends Component {
     const campaign = Campaign(address)
     const requestCount = await campaign.methods.getRequestsCount.call()
     const requests = await Promise.all(
-      Array(requestCount)
+      Array(parseInt(requestCount))
         .fill()
         .map((el, index) => {
           return campaign.methods.requests(index).call()
@@ -19,6 +19,7 @@ class RequestIndexPage extends Component {
     return { address, requests, requestCount }
   }
   render() {
+    const { Header, Row, HeaderCell, Body } = Table
     const { address } = this.props
     return (
       <Layout>
@@ -28,6 +29,18 @@ class RequestIndexPage extends Component {
             <Button primary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
       </Layout>
     )
   }
